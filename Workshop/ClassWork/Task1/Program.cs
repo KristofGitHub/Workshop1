0cb1846659830db44dd0af701cd0b3ba14da664e
Vasiliev Sergei <sergei.binp@gmail.com>
2023-04-21 23:39:58 +0500

    2nd commit: add Task_1 for square of input number

--- Workshop/ClassWork/Task1/Program.cs
+++ Workshop/ClassWork/Task1/Program.cs
@@ -1,3 +1,8 @@
-﻿Console.WriteLine("Input number: ");
+﻿// Console.WriteLine("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"You are input {num} number");
+
+// Task_1: take number and output nember*numer
+Console.WriteLine("Input number: ");
 int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"You are input {num} number");
+Console.WriteLine($"You are input {num*num} number");
