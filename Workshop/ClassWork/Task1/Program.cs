8ac8cb35500d550811662f0f419098bc42d004b8
Vasiliev Sergei <sergei.binp@gmail.com>
2023-04-21 23:49:44 +0500

    3st commit: add Task_2

--- Workshop/ClassWork/Task1/Program.cs
+++ Workshop/ClassWork/Task1/Program.cs
@@ -3,6 +3,17 @@
 // Console.WriteLine($"You are input {num} number");
 
 // Task_1: take number and output nember*numer
-Console.WriteLine("Input number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"You are input {num*num} number");
+// Console.WriteLine("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"You are input {num*num} number");
+
+// Task_2: Является ли первое число квадратом второго?
+Console.WriteLine("Input 1st number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input 2nd number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num2*num2 == num1){
+    Console.WriteLine($"Число {num1} является квадратом числа {num2}");
+}else{
+    Console.WriteLine($"Число {num1} НЕ является квадратом числа {num2}");
+}
