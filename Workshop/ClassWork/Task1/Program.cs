adc02cfb98ffb249a84789b2c6636d70938f3703
Vasiliev Sergei <sergei.binp@gmail.com>
2023-04-22 11:13:33 +0500

    4st commit: add Task_3 about serial from -n to n

--- Workshop/ClassWork/Task1/Program.cs
+++ Workshop/ClassWork/Task1/Program.cs
@@ -8,12 +8,22 @@
 // Console.WriteLine($"You are input {num*num} number");
 
 // Task_2: Является ли первое число квадратом второго?
-Console.WriteLine("Input 1st number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input 2nd number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-if (num2*num2 == num1){
-    Console.WriteLine($"Число {num1} является квадратом числа {num2}");
-}else{
-    Console.WriteLine($"Число {num1} НЕ является квадратом числа {num2}");
+// Console.WriteLine("Input 1st number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input 2nd number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num2*num2 == num1){
+//     Console.WriteLine($"Число {num1} является квадратом числа {num2}");
+// }else{
+//     Console.WriteLine($"Число {num1} НЕ является квадратом числа {num2}");
+// }
+
+// Task_3: Принимаем N и выдаем последовательность от -N до N.
+Console.WriteLine("Input number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int M = -N;
+while (M != N + 1){
+    Console.WriteLine(M);
+    M++;
 }
+
